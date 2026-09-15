9f154ab8e2b3b157fbaa95b1bde1fc84f4bbbcd1
sangmatony5271-sketch <sangmatony5271@gmail.com>
2026-09-16 00:11:28 +0600

    Add Gradle wrapper files

--- gradlew.bat
+++ gradlew.bat
@@ -1,20 +1,4 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
-
-@if "%DEBUG%"=="" @echo off
+@if "%DEBUG%"==\"\" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
@@ -25,7 +9,7 @@
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
+if "%DIRNAME%"==\"\" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
@@ -51,7 +35,7 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_HOME=%JAVA_HOME:\"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
@@ -69,14 +53,26 @@ goto fail
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-@endlocal & set ERROR_CODE=%ERRORCODE%
+@endlocal & set ERROR_CODE=%ERRORLEVEL%
 
-if not "%ERRORCODE%"=="0" goto fail
+if not "%TEMP%"==\"\" (
+    del /Q "%TEMP%\gradle-%RANDOM%.tmp" 2>nul
+)
+
+if %ERROR_CODE% equ 0 goto mainEnd
 
 :fail
-exit /b %ERRORCODE%
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the wrapper batch code.
+rem set GRADLE_EXIT_CONSOLE=true
+if NOT "x%GRADLE_EXIT_CONSOLE%"=="x" exit 1
+exit /b %ERROR_CODE%
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
